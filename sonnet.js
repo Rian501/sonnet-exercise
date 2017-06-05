5175fcb840aaa4174d2f3f373050b91b65ff5b04
Rian501 <eclaire.lemmon@gmail.com>
2017-06-05 15:45:30 -0500

    Update sonnet to replacement text

--- sonnet.js
+++ sonnet.js
@@ -1,30 +1,39 @@
 
 // Take the contents of the sonnet div and place it in a variable.
 
-var sonnetText = ""
+const sonnet = document.getElementById('sonnet');
+let sonnetText = sonnet.innerHTML;
+
+console.log("sonnet", sonnetText);
 
 
 // Find and output the starting position of the word "orphans".
 
+var orphanIndex = sonnetText.indexOf("orphans")
 
+console.log("index of Orphans", orphanIndex);
 
 
 
 // Output the number of characters in the sonnet.
 
-
+var sonnetLength = sonnetText.length;
+console.log("sonnet text length", sonnetLength);
 
 
 // Replace the first occurance of the string "winter" with "yuletide".
 
-
+var sonnetUpdate = sonnetText.replace("winter", "yuletide");
+console.log("replce text", sonnetUpdate);
 
 
 // Replace all occurances of the string "the" with "a large".
 
 
+sonnetUpdate = sonnetUpdate.replace(/the /g, "a large ");
+console.log("replace more text", sonnetUpdate);
 
 
 // Set the content of the sonnet div with the new string.
 
-
+sonnet.innerHTML = sonnetUpdate;
